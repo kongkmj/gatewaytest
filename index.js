7416cccc5b53425da85dfd7e9cd046179a73aad8
kongkmj <asd789zx@naver.com>
2016-08-02 14:47:20 +0900

    tcp g test

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ var bodyParser = require('body-parser');
 require('events').EventEmitter.prototype._maxListeners = 0;
 
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var net = require('net');
-
 var recieveData;
+var sendData;
 
 var server = net.createServer(function (socket) {
   console.log(socket.address().address+"connected");
 
+
+
   //client로 부터 오는 data 출력
   socket.on('data',function (data) {
-    console.log('recieve: '+data);
-    recieveData=data;
+    recieveData=""+data;
+    console.log(recieveData);
+    io.emit('tcptest',recieveData);
   });
 
   //client와 접속이 끊겻을때
@@ -27,6 +31,13 @@ var server = net.createServer(function (socket) {
   });
   //client 가 접속 했을때
   socket.write('welcome to TCP server');
+
+
+io.on('connection', function (socket2) {
+      socket2.on('senddata3',function(msg){
+      socket.write(msg);
+    });
+});
 
 });
 
@@ -56,16 +67,23 @@ var tcptext="";
 
 
 app.get('/',function (req,res) {
-  res.render('index',{data:recieveData});
+  res.render('index');
 
 });
 app.post('/input',function (req,res) {
     console.log(req.body.test);
     tcptext=req.body.test;
-    res.redirect('/');
+    io.emit('senddata1',tcptext);
 });
 
+io.on('connection', function (socket2) {
+    //chat message 이벤트 발생
+    socket2.on('senddata',function(msg){
+        //public 통신
+        io.emit('senddata',msg);
+    });
+});
 
-app.listen(3000,function () {
+http.listen(3000,function () {
   console.log('server listening at port 3000');
 });
